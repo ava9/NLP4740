fa1767d12135c17ae52068b8de1acaafff7b442d
Rohit <rohit@dhcp-hol-4845.redrover.cornell.edu>
2016-09-11 22:03:40 -0400

    add bigram sentence generation and demo capability

--- p1.py
+++ p1.py
@@ -1,11 +1,9 @@
-from nltk import word_tokenize
+import nltk
 import re
 import os
 import random
 
 endingChars = ".!?;"
-
-
 
 def preprocesssContent(content):
   emailPattern = '[\w\.-]+@[\w\.-]+'
@@ -19,13 +17,10 @@ def preprocesssContent(content):
   content = content.split('Re : ')[-1]
   content = content.split('wrote : ')[-1]
       
-  removedChars = ",][\^_`()}{*+-#\$%&/<=>@|~<>\|\\"
+  removedChars = "][\^_`()}{*+-#\$%&/<=>@|~<>\|\"\\"
 
   badCharReg = re.compile('[%s]' % re.escape(removedChars))
   content = badCharReg.sub('', content)
-
-  multipleSpaces = '[\s]+'
-  content = re.sub(multipleSpaces, ' ', content)
 
   return content
 
@@ -37,7 +32,7 @@ def getFileContents(folderName, fileNumber):
   if os.path.exists(fileName):
     with open(fileName) as f:
       content = f.read()
-      return word_tokenize(preprocesssContent(content))
+      return nltk.word_tokenize(preprocesssContent(content))
   return []
 
 def updateDict(tokens, d):
@@ -46,40 +41,90 @@ def updateDict(tokens, d):
       d[token] += 1
     else:
       d[token] = 1
+  return d
 
 def totalsToCDFTokenArray(d):
   cdfArray = []
   for token in d:
-    for i in range(d[token]-1):
+    for i in range(d[token]):
       cdfArray.append(token)
   return cdfArray
 
-def getUnigramDict(folderName):
+def bigramPreprocess(tokens):
+  newTokens = ["|"]
+  for token in tokens:
+    newTokens.append(token)
+    if token in endingChars:
+      newTokens.append("|")
+
+  #might not be needed
+  if newTokens[-1] != "|":
+    newTokens.append("|")
+
+  return newTokens
+
+def getDict(folderName, isUnigram):
   UDict = dict()
   for fileNumber in range(300):
+
     tokens = getFileContents(folderName, fileNumber)
-    updateDict(tokens, UDict)
+    if not isUnigram:
+      tokens = list(nltk.bigrams(bigramPreprocess(tokens)))
+    UDict = updateDict(tokens, UDict)
   return UDict
 
-def unaryGenRandWord(cdfArray):
+def genRandWord(cdfArray):
   i = random.randint(0,len(cdfArray)-1)
   return cdfArray[i]
 
-
-def generateUnigramSentence(folderName):
-  cdfArray = totalsToCDFTokenArray(getUnigramDict(folderName))
-
+def generateUnigramSentence(cdfArray):
   #get starting word
-  word = unaryGenRandWord(cdfArray)
+  word = genRandWord(cdfArray)
   while word in endingChars: 
-    word = unaryGenRandWord(cdfArray)
+    word = genRandWord(cdfArray)
 
   sentence = word
   #keep going till we get a ending symbol
   while word not in endingChars:
-    word = unaryGenRandWord(cdfArray)
+    word = genRandWord(cdfArray)
     sentence += " " + word
   return sentence
 
-mydict = generateUnigramSentence('motorcycles')
-print mydict
+def generateBigramSentence(cdfArray):
+  #get starting word
+  tupl = genRandWord(cdfArray)
+  #might need to change this
+  while tupl[0] != "|" or tupl[1] in endingChars: 
+    tupl = genRandWord(cdfArray)
+
+  sentence = tupl[1]
+  #keep going till we get a ending symbol
+  while tupl[1] != "|" and tupl[0] not in endingChars:
+    oldWord = tupl[1]
+    tupl = genRandWord(cdfArray)
+    while tupl[0] != oldWord: 
+      # print oldWord
+      # print sentence
+      tupl = genRandWord(cdfArray)
+    sentence += " " + tupl[1]
+  return sentence[:-2]
+
+def demo():
+  uniCDFArray = totalsToCDFTokenArray(getDict('motorcycles', 1))
+
+  print "Unigram Sentences:"
+  for i in range(10):
+    print generateUnigramSentence(uniCDFArray)
+  print "\n\n\n"
+  biCDFArray = totalsToCDFTokenArray(getDict('motorcycles', 0))
+  print "Bigram Sentences:"
+  for i in range(10):
+    print generateBigramSentence(biCDFArray)
+
+def test():
+  biCDFArray = totalsToCDFTokenArray(getDict('motorcycles', 0))
+  while 1:
+    print generateBigramSentence(biCDFArray)
+
+demo()
+# test()
