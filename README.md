5aac18625bffdbe86656043bd4e6931bcb8697e3
Rohit <rohit@dhcp-rhodes-932.redrover.cornell.edu>
2016-09-12 19:51:36 -0400

    sexy readme and comments yo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NLP4740
+
+In order to run the demo, please run:
+>> python p1.py
+
+This will generate 10 unigram sentences and 10 bigram sentences from a random corpus
--- p1.py
+++ p1.py
@@ -5,12 +5,14 @@ import random
 
 endingChars = ".!?;"
 
+#gets the corpora for the sentence generation task, removes OSX hidden file and classification task for now
 def getCorpora():
   corpora = os.listdir("data_corrected/classification task/")
   corpora.remove(".DS_Store")
   corpora.remove("test_for_classification")
   return corpora
 
+#given the content of a file in a string, preprocesses it to only have relevant words for the corpus
 def preprocesssContent(content):
   #only keep the actual contents of the article
   content = content.split('writes : ')[-1]
@@ -32,9 +34,12 @@ def preprocesssContent(content):
 
   return content
 
+#returns the file path of a document given the corpus and the file number
 def getFileName(folderName, fileNumber):
   return "data_corrected/classification task/" + folderName + "/train_docs/" + folderName + "_file{}.txt".format(fileNumber)
 
+#gets the tokens of a file given a corpus and file number (by preprocessing and tokenizing)
+#returns the tokens if the file exists, or an empty array otherwise
 def getFileContentTokens(folderName, fileNumber):
   fileName = getFileName(folderName, fileNumber)
   if os.path.exists(fileName):
@@ -43,6 +48,8 @@ def getFileContentTokens(folderName, fileNumber):
       return nltk.word_tokenize(preprocesssContent(content))
   return []
 
+#updates a frequency table dictionary of n-grams given the old dictionary and the new n-grams,
+#returns the new dictionary
 def updateDict(keys, d):
   for k in keys:
     if j in d:
@@ -51,6 +58,8 @@ def updateDict(keys, d):
       d[k] = 1
   return d
 
+#creates an array from an n-gram dictionary such that the number of occurances 
+#of the n-gram in the array is equal to the frequency in the corpus
 def totalsToPDFTokenArray(d):
   pdfArray = []
   for key in d:
@@ -58,6 +67,7 @@ def totalsToPDFTokenArray(d):
       pdfArray.append(key)
   return pdfArray
 
+#special preprocessing for bigrams
 def bigramPreprocess(tokens):
   newTokens = ["|"]
   for token in tokens:
@@ -68,9 +78,9 @@ def bigramPreprocess(tokens):
   #might not be needed
   if newTokens[-1] != "|":
     newTokens.append("|")
-
   return newTokens
 
+#assembles dictionary for up to 300 documents in a corpus
 def getDict(folderName, isUnigram):
   d = dict()
   for fileNumber in range(300):
@@ -80,53 +90,63 @@ def getDict(folderName, isUnigram):
     d = updateDict(tokens, d)
   return d
 
+#gets a random (uniform) word from a pdf array as described above
 def genRandWord(pdfArray):
   return pdfArray[random.randint(0,len(pdfArray)-1)]
 
+#generates a unigram sentence
 def generateUnigramSentence(pdfArray):
-  #get starting word
+  #get starting word (that isn't an ending character)
   word = genRandWord(pdfArray)
   while word in endingChars: 
     word = genRandWord(pdfArray)
 
   sentence = word
-  #keep going till we get a ending symbol
+  #keep going until we get a ending symbol
   while word not in endingChars:
     word = genRandWord(pdfArray)
     sentence += " " + word
   return sentence
 
+#generates a bigram sentence
 def generateBigramSentence(pdfArray):
   #get starting word
   tupl = genRandWord(pdfArray)
+  #makes sure the first token in the bigram is a beginning character
   while tupl[0] != "|" or tupl[1] in endingChars: 
     tupl = genRandWord(pdfArray)
 
   sentence = tupl[1]
-  #keep going till we get a ending symbol
+  #keep going till we get an end of sentence symbol/endChar (same logic)
   while tupl[1] != "|" and tupl[0] not in endingChars:
     oldWord = tupl[1]
+    #makes sure a new random word is generated, in case of a bigram where the tokens are the same
     tupl = genRandWord(pdfArray)
     while tupl[0] != oldWord: 
       tupl = genRandWord(pdfArray)
     sentence += " " + tupl[1]
   return sentence[:-2]
 
+#main demo of sentence generation
 def demo():
+  #get all the corpora and choose a random one
   corpora = getCorpora()
   corpusToUse = corpora[random.randint(0,len(corpora)-1)]
 
+  #generate 10 unigram sentences
   unipdfArray = totalsToPDFTokenArray(getDict(corpusToUse, 1))
 
   print "\nUnigram Sentences with the " + corpusToUse + " corpus:"
   for i in range(10):
     print generateUnigramSentence(unipdfArray)
 
+  #generate 10 bigram sentences
   bipdfArray = totalsToPDFTokenArray(getDict(corpusToUse, 0))
   print "\n\n\nBigram Sentences with the " + corpusToUse + " corpus:"
   for i in range(10):
     print generateBigramSentence(bipdfArray)
 
+#testing how reliable our corpus is. Test function, can be ignored
 def test():
   bipdfArray = totalsToPDFTokenArray(getDict('autos', 0))
   while 1:
