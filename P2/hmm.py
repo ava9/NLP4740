20cbfc2246adb483b9d6b1c4fd4851974559653e
Aditya Agashe <ava9@users.noreply.github.com>
2016-10-22 15:26:52 -0400

    pushing hmm

--- P2/hmm.py
+++ P2/hmm.py
@@ -1,90 +1,96 @@
 import sys
 import glob
-from hmmlearn.hmm import GaussianHMM
-from hmmlearn.hmm import MultinomialHMM
+#from hmmlearn.hmm import GaussianHMM
+#from hmmlearn.hmm import MultinomialHMM
 import numpy as np
-from seqlearn.perceptron import StructuredPerceptron
+from seqlearn.hmm import MultinomialHMM
 
 
-def stringToInt(string):
-	return int(''.join(str(ord(c)) for c in string))
+def toIntArr(strarr):
+	uniqueW, locs = np.unique(strarr, return_inverse = True)
+	return (locs.reshape(-1,1) == np.arange(len(uniqueW))).astype(int)
 
 def createFeatures(path, isTrain):
 	files = glob.glob(path)   
 	featureArray = []
+	lengths = []
+	currLen = 0
 	for name in files:
-		try:
-			with open(name) as f:
-				for line in f:
-					line = line.strip('\n')
-					if line:
-						fields = line.split("\t")
-						if (len(fields) > len(featureArray)):
-							for i in range(0, len(fields)):
-								featureArray.append([])
-						else:
-							pass
-						for i in range(0, len(featureArray)):
-							featureArray[i].append(fields[i])
-					else:
-						pass
-		except IOError as exc:
-			if exc.errno != errno.EISDIR: 
-				raise
-			else:
-				pass
+		with open(name) as f:
+			fd = f.read()
+			for line in fd:
+				print (fd)
+				print '.....'
+				line = line.strip('\n')
+				if line and len(line) > 0:
+					fields = line.split('\t')
+					featureArray.append(fields)
+					currLen += 1
+				else:
+					if currLen > 0:
+						lengths.append(currLen)
+						currLen = 0
+			if currLen > 0:
+				lengths.append(currLen)
+				currLen = 0
 
-	features = []
-	words = np.array([stringToInt(l[0]) for l in featureArray])
-	features.append(words)
-	pos = np.array([stringToInt(l[1]) for l in featureArray])
-	features.append(pos)
-	tags = np.array([stringToInt(l[2]) for l in featureArray]) #need to change this to BILOU tags
-	tags = []
+	tags = np.array([])
+
+	tempTags = []
 	currCue = -1
-	taggggggs = np.array([])
 	if isTrain:
 		for i in range(len(featureArray)):
 			t = featureArray[i][2]
 			if t[0] == 'C':
 				if t[-1] == currCue:
-					tags.append('I')
+					tempTags.append('I')
 				else:
-					tags.append('B')
+					tempTags.append('B')
 					currCue = t[-1]
 			else:
 				currCue = -1
-				if len(tags) != 0:
-					if tags[-1] == 'B': 
-						tags[-1] = 'U'
-					elif tags[-1] == 'I': 
-						tags [-1] = 'L'
-				tags.append('O')
+				if len(tempTags) != 0:
+					if tempTags[-1] == 'B': 
+						tempTags[-1] = 'U'
+					elif tempTags[-1] == 'I': 
+						tempTags [-1] = 'L'
+				tempTags.append('O')
 
-			if tags[-1] == 'B': 
-				tags[-1] = 'U'
-			elif tags[-1] == 'I': 
-				tags[-1] = 'L'
+			if tempTags[-1] == 'B': 
+				tempTags[-1] = 'U'
+			elif tempTags[-1] == 'I': 
+				tempTags[-1] = 'L'
+		#make better stringtoint
+		tags = np.array(toIntArr(tempTags))
 
-		taggggggs = np.array([stringToInt(tags[l])] for l in tags)
 		# features.append(taggggggs)
-	return (features, taggggggs)
+	words = [row[0] for row in featureArray]
+	pos = [row[1] for row in featureArray]
+	words = toIntArr(words)
+	pos = toIntArr(pos)
+	featureArray = zip(words, pos)
+	print "EXTRACTED FEATURES"
+	return (np.array(featureArray), tags, lengths)
 
 
 def read(trainPath='train/*.txt', testPath='test-public/*txt'):  
-	(X, y) = createFeatures(trainPath, 1)
+	(X, y, lengths1) = createFeatures(trainPath, 1)
 	#model = GaussianHMM(n_components=2, covariance_type='spherical', params=set(X.ravel())).fit(X)
 	#model = MultinomialHMM(n_components=3).fit(X)
 	#fo = open("hmmModel.model",'w')
 	#o.write('%s' % model)
-	model = StructuredPerceptron()
-	model.fit(X, y, [4])
-	(X2, y2) = createFeatures(testPath, 0)
+	model = MultinomialHMM()
+	#print("X is:" str(X))
+	#print("Y is" + str(y))
+	#print("lengths is: " + str(lengths1))
+	print "TRAINING MODEL"
+	# model.fit(X, y, lengths1)
+	# (X2, y2,lenghts2) = createFeatures(testPath, 0)
 
-	test = model.predict(X2, [4])
+	# test = model.predict(X2, lengths2)
 
 
-	print(str(test))
+	# print(str(test))
 	#test = model.predict(Y)
 	#print(test)
 	#print(X.ravel())
