32f82956360b964fc04d5ed0c1f981f1d3ad9869
johnhughes96 <jlh457@cornell.edu>
2016-11-02 18:08:40 -0400

    parse question file

---
+++ P3/parseQuestion.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+
+def getQuestions():
+	questionDict = dict()
+
+	f = open('question.txt','r')
+
+	lastQID = 0
+	for line in f.readlines():
+		line = line.strip('\n')
+
+		if line:
+			pieces = line.split(' ')
+			if pieces[0] == '<num>':
+				qid = int(pieces[2])
+				questionDict[qid] = ''
+				lastQID = qid
+			elif pieces[0][0] != '<':
+				questionDict[lastQID] = line
+
+	return questionDict
+
+if __name__ == '__main__':
+	print getQuestions()
+
